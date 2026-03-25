d0d3e7c4a4703649e5966857c31226621229e470
fernandaaime <fe.aime23@gmail.com>
2026-03-25 20:41:53 -0300

    fix: aumentar timeout para execucao em pipeline

--- src/test/java/com/agi/BlogAgiTest.java
+++ src/test/java/com/agi/BlogAgiTest.java
@@ -19,14 +19,15 @@ public class BlogAgiTest {
     public void setUp() {
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--start-maximized");
         options.addArguments("--headless");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--disable-gpu");
         options.addArguments("--window-size=1920,1080");
+        options.addArguments("--remote-allow-origins=*");
         driver = new ChromeDriver(options);
-        wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(60));
+        wait = new WebDriverWait(driver, Duration.ofSeconds(30));
     }
 
     @Test(description = "CT01 - Busca por termo valido retorna resultados")
